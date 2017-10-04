7a4739f07c8b7a145aa16494f147335a879e0de8
AlexHanrath <32524249+AlexHanrath@users.noreply.github.com>
2017-10-04 21:41:53 +0200

    Update Population.java

--- simulation/Population.java
+++ simulation/Population.java
@@ -1,10 +1,17 @@
 package simulation;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
 public class Population {
 	
 	private Organism type;
 	private List<Organism> pool = new ArrayList<Organism>();
 	private Consumer<List<Organism>> onUpdate;
+	
+	private double mutationChance = 0.01;
+	private double selectionPressure = 0.01;
 	
 	public Population(Organism type) {
 		
@@ -22,6 +29,22 @@ public class Population {
 		
 	}
 	
+	public void setMutationChance(double newValue) {
+		mutationChange = newValue;	
+	}
+	
+	public double getMutationChance(double newValue) {
+		return mutationChange;
+	}
+	
+	public void setSelectionPressure(double newValue) {
+		selectionPressure = newValue;	
+	}
+	
+	public double getSelectionPressure(double newValue) {
+		return selectionPressure;	
+	}
+	
 	private void update() {
 		
 		for (Organism o : pool) {
@@ -35,6 +58,12 @@ public class Population {
 	}
 	
 	private void select() {
+		
+		//TODO
+		
+	}
+	
+	private void initPool() {
 		
 		//TODO
 		
